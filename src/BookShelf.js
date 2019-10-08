138cf5a9ca9e94ec70e044e6193cafe69840844c
hannahdettmann <32919333+dettma1@users.noreply.github.com>
2019-10-08 11:29:40 -0500

    partitioned files

---
+++ src/BookShelf.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Book from './Book'
+import *  as BooksAPI from './BooksAPI';
+
+class BookShelf extends React.Component{
+    render()    {
+        return (
+            <div className="bookshelf">
+            <h2 className="bookshelf-title">{this.props.title}</h2>
+            <div className="bookshelf-books">
+              <ol className="books-grid">
+                  {this.props.books.map((book) => 
+                    myBook = 
+                      <li>
+                          <Book title={book.title} author={book.author} url={book.url}/>
+                      </li>
+                  )}
+              </ol>
+            </div>
+          </div>
+        )
+
+    }
+}
+
+export default BookShelf;
